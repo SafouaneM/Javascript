19c5c48ae963222dac351b1f78cab85dd648ec45
SafouaneM <safouane_m@Hotmail.nl>
2017-12-12 09:16:27 +0100

    Javascript DOM

---
+++ Vulmetjs/vulmetjs.js
@@ -0,0 +1,4 @@
+document.getElementById("paginatitel").innerText = "Mijn geweldige pagina";
+document.getElementById("subkop").innerText = "Mijn geweldige subkop";
+document.getElementById("Censio").src = "../Censio.jpg";
+document.getElementById("Sensio").src = "../Sensio.jpg";
