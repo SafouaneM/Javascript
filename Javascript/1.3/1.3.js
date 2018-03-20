cf0232b7e2ef5b414116a05ddd20822831d4fe23
Safouane <31611670+SafouaneM@users.noreply.github.com>
2018-03-20 09:30:45 +0100

    Add files via upload

---
+++ Javascript/1.3/1.3.js
@@ -0,0 +1,9 @@
+var mijnauto = {
+    merk: "Ford",
+    type: "Mondeo",
+    aantalWielen: 4,
+    kleur: "Blauw"
+}
+
+console.log(mijnauto.kleur);
+document.getElementById("Grotekop").innerHTML = mijnauto.kleur
